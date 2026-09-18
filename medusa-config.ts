eb8b024ffdd6bb16b543bc12970a9e4e2b819317
akdi3679 <3kadiak679@gmail.com>
2026-09-18 02:05:59 +0200

    Hardcode disable: true for pure API backend

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,7 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   admin: {
-    disable: process.env.ADMIN_DISABLED === "true",
+    disable: true,
     backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
   },
   

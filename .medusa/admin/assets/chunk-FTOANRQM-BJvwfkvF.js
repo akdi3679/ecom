ce3f187ff6e05aff201d32c6ede2af89e154b022
akdi3679 <3kadiak679@gmail.com>
2026-09-19 11:01:27 +0200

    fix: force add missing admin build files to git

---
+++ .medusa/admin/assets/chunk-FTOANRQM-BJvwfkvF.js
@@ -0,0 +1,1 @@
+import{bc as i}from"./index-BsUcY7ra.js";var e=i("product","*categories,*shipping_profile,-variants");export{e as P};
---
+++ .medusa/admin/assets/chunk-MBMHGW6Y-D93uHik1.js
@@ -0,0 +1,1 @@
+import{eQ as r}from"./index-BsUcY7ra.js";r("sales_channel_status",(e,a,l)=>({color:e?"grey":"green",label:l(e?"general.disabled":"general.enabled")}));
